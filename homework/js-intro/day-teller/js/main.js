35a7f5e8c18ed9d223411b66a26c741b9332b2e2
Daawa-Karma <theo.leveque@gmail.com>
2020-12-06 12:44:03 -0800

    background picker and day-teller done

---
+++ homework/js-intro/day-teller/js/main.js
@@ -0,0 +1,18 @@
+const check = document.querySelector('#check');
+const result = document.querySelector('#placeToSee');
+check.addEventListener('click', () => {
+	const day = document.querySelector('#day').value.toLowerCase();
+	switch (day) {
+		case 'tuesday':
+		case 'thursday':
+			result.innerText = "It's class day!";
+			break;
+		case 'saturday':
+		case 'sunday':
+			result.innerText = "It's the week-end!";
+			break;
+		default:
+			result.innerText = 'Just another work day!';
+			break;
+	}
+});
