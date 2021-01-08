4c4cedbff825ea9b420643e1a3f5d3b4dcd566c4
Daawa-Karma <theo.leveque@gmail.com>
2021-01-07 17:44:33 -0800

    first part done and removed useles files

--- class14/day-checker/js/main.js
+++ class14/day-checker/js/main.js
@@ -6,8 +6,28 @@
 //If Monday and House Hamilton, tell them to book a 1on1
 //If any other day or wrong house, tell them to study
 
+let check = document.querySelector('h1');
+let p = document.querySelector('p');
 
+const checkIt = () => {
+	const day = document.querySelector('#whatDay').value.toLowerCase();
+	const house = document.querySelector('#whatHouse').value.toLowerCase();
+	if (day === 'tuesday' || day === 'thursday') {
+		p.innerText = 'You have Class';
+	} else if (day === 'saturday') {
+		p.innerText = 'Office Hours today';
+	} else if (day === 'friday' && house === 'turing') {
+		p.innerText = 'You got private office hours today';
+	} else if (day === 'wednesday' && house === 'hopper') {
+		p.innerText = 'You got private office hours today';
+	} else if (day === 'monday' && house === 'hamilton') {
+		p.innerText = 'Book a one on one!';
+	} else {
+		p.innerText = 'Study Time!';
+	}
+};
 
+check.addEventListener('click', checkIt);
 //--- Harder
 //On click of the h1
 //Take the value from the input
--- class14/variable-fun-time/js/main.js
+++ class14/variable-fun-time/js/main.js
@@ -1,20 +1,27 @@
 //--- Easy
 //create a variable and assign it a boolean
-
+let isCool = true;
 //create a variable and assign it a string that is a number
-
+let numberString = '4';
 //add 10 to that number - how?!? https://media.giphy.com/media/lkdH8FmImcGoylv3t3/giphy.gif
-
+numberString = parseInt(numberString) + 10;
 //print that number to the console
-
+console.log(numberString);
 //--- Medium
 //create a variable that holds a value from the input
 
-//add 21 to that number
-
-//alert that number
-
+document.querySelector('#button').addEventListener('click', () => {
+	let inputValue = document.querySelector('#field').value;
+	//add 21 to that number
+	inputValue = Number(inputValue) + 21;
+	//alert that number
+	alert(inputValue);
+});
 //--- Hard
 //create a variable that holds the h1
-
-//add an event listener to that element that places the product of the two previous variables in the DOM
+// let h1 = document.querySelector('h1');
+// //add an event listener to that element that places the product of the two previous variables in the DOM
+// let h2 = document.querySelector('h2');
+// h1.addEventListener('click', () => {
+// 	h2.textContent = inputValue * numberString;
+// });
