13c690480c0a35f2d09b1e631706e46b156f26fc
Daawa-Karma <theo.leveque@gmail.com>
2021-01-07 16:32:17 -0800

    after holidays commit

--- homework/js-intro/case/main.js
+++ homework/js-intro/case/main.js
@@ -20,3 +20,67 @@ const convert = () => {
 };
 
 btn.addEventListener('click', convert);
+
+const encode = string => {
+	let result = [];
+	string.split('').map(l => {
+		switch (l) {
+			case 'a':
+				l = 1;
+				result.push(l);
+				break;
+			case 'e':
+				l = 2;
+				result.push(l);
+				break;
+			case 'i':
+				l = 3;
+				result.push(l);
+				break;
+			case 'o':
+				l = 4;
+				result.push(l);
+				break;
+			case 'u':
+				l = 5;
+				result.push(l);
+				break;
+			default:
+				l = l;
+				result.push(l);
+		}
+	});
+	return result.join('');
+};
+
+const decode = string => {
+	let result = [];
+	string.split('').map(l => {
+		switch (l) {
+			case '1':
+				l = 'a';
+				result.push(l);
+				break;
+			case '2':
+				l = 'e';
+				result.push(l);
+				break;
+			case '3':
+				l = 'i';
+				result.push(l);
+				break;
+			case '4':
+				l = 'o';
+				result.push(l);
+				break;
+			case '5':
+				l = 'u';
+				result.push(l);
+				break;
+			default:
+				l = l;
+				result.push(l);
+		}
+	});
+	return result.join('');
+};
