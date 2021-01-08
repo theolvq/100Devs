a40fed08c8e837fab8cd9695faf4220e271bd624
Daawa-Karma <theo.leveque@gmail.com>
2021-01-07 18:34:12 -0800

    class14 done

--- class14/bring-it-on/js/main.js
+++ class14/bring-it-on/js/main.js
@@ -1,17 +1,32 @@
 // *Variables*
 // Create a variable and alert log the value
-
+const love = true;
+alert(love);
 // Create a variable, add -15 to it, and print the value to the console
-
+let ten = 10;
+console.log(ten - 15);
 // *Functions*
 // Create a function that multiplys 4 numbers and alerts the product
-
+const mutl4 = (n1, n2, n3, n4) => alert(n1 * n2 * n3 * n4);
 // Create a function that divides one number by another and returns the remainder plus 10
-
+const div = (n1, n2) => (n1 % n2) + 10;
 // *Conditionals*
 // Create a function that adds three numbers and if the sum is greater than 1000 alert "Block Party"
-
+const add3 = (n1, n2, n3) => {
+	let sum = n1 + n2 + n3;
+	if (sum > 1000) {
+		alert('Block Party');
+	}
+};
 // Create a function that divides five numbers and if the quotient is divisible by 5 alert LINCOLN
-
+const div5 = (n1, n2, n3, n4, n5) => {
+	let quotient = n1 / n2 / n3 / n4 / n5;
+	if (quotient % 5) alert('Lincoln');
+};
 //*Loops*
 //Create a function that takes in a word and a number. Console log the word x cubed times where x was the number passed in
+const weird = (str, num) => {
+	for (let i = 0; i < Math.pow(num, 2); i++) {
+		console.log(str);
+	}
+};
--- class14/simple-functions/js/main.js
+++ class14/simple-functions/js/main.js
@@ -1,12 +1,26 @@
 //---Easy
 //create a function that adds two numbers and alerts the difference
-
+const addTwo = (n1, n2) => alert(n1 - n2);
 //create a function that divides two numbers and console logs the quotient
-
+const divTwo = (n1, n2) => console.log(n1 / n2);
 //create a function that multiplys four numbers and returns the product
-
+const multFour = (n1, n2, n3, n4) => n1 * n2 * n3 * n4;
 //---Medium
 //create a function that takes in four numbers. Add the first two numbers and return the remainder of dividing the sum of the first two numbers by the difference of the last two numbers
-
+const fourNum = (n1, n2, n3, n4) => {
+	let sum = n1 + n2;
+	let dif = n3 - n4;
+	return sum % dif;
+};
 //---Hard
 //create a function that takes in 4 numbers. Multiply the first two numbers. If the product is greater than 100 add the sum of the last two numbers and console log the value. If the product is less that 100, subtract the difference of the last two numbers and console log the value. If the product is 100, multiply the first three numbers together and alert the remainder of dividing the fourth number
+const fourNum2 = (n1, n2, n3, n4) => {
+	let prod = n1 * n2;
+	if (prod > 100) {
+		console.log(n3 + n4);
+	} else if (prod < 100) {
+		console.log(n3 - n4);
+	} else if (prod === 100) {
+		alert((n1 * n2 * n3) % n4);
+	}
+};
