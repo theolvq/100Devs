03ec98fc125a1f928b6bee70053a1041578415d4
Daawa-Karma <theo.leveque@gmail.com>
2021-01-12 20:27:30 -0800

    class15 and lab done

--- class15/arrays-in-it-to-win-it/js/main.js
+++ class15/arrays-in-it-to-win-it/js/main.js
@@ -1,7 +1,13 @@
 //Create an array of movies with at least three movies.
-
+const movies = [
+	'jurassic park',
+	'the matrix',
+	'the lord of the rings',
+	'footloose',
+];
 //Using the array from above, store the first movie in a variable
-
+const movieOne = movies[0];
 //Get the length of the original array and store it in a new variable
-
+const arraylength = movies.length;
 //Get the last element in that array and store it in a new variable. What if your array was really large and you didn't know the last index? Would your solution still work?
+const last = movies[movies.length - 1];
