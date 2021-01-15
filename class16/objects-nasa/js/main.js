3449c4c7f2bc2c47bd757305b4156212e24d4d68
Daawa-Karma <theo.leveque@gmail.com>
2021-01-15 11:44:38 -0800

    nasa API plugged in

--- class16/objects-nasa/js/main.js
+++ class16/objects-nasa/js/main.js
@@ -1,1 +1,14 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+const img = document.querySelector('img');
+const date = document.querySelector('input').value;
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', () => {
+	fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`)
+		.then(res => res.json())
+		.then(data => {
+			console.log(data);
+			img.src = data.url;
+		})
+		.catch(err => console.log(`error ${err}`));
+});
