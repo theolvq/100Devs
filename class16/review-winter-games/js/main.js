53573b502f8d1d3319ddaa5a41c0e9c6ce91f1f9
Daawa-Karma <theo.leveque@gmail.com>
2021-01-14 18:03:13 -0800

    lab05 done

--- class16/review-winter-games/js/main.js
+++ class16/review-winter-games/js/main.js
@@ -1,2 +1,12 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
 const evenNums = array => array.filter(x => x % 2 === 0);
+
+const evenArr = array => {
+	let newArray = [];
+	array.forEach(element => {
+		if (element % 2 === 0) {
+			newArray.push(element);
+		}
+	});
+	return newArray;
+};
