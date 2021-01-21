bc9dd24e3901b6ee2efc2cfce929f8e0cfe4e719
Daawa-Karma <theo.leveque@gmail.com>
2021-01-21 15:49:55 -0800

    class18 started

--- class17/objects-zeo/js/main.js
+++ class17/objects-zeo/js/main.js
@@ -1,1 +1,11 @@
 //Create a stopwatch object that has four properties and three methods
+let stopwatch = {};
+
+stopwatch.color = 'black';
+stopwatch.brand = 'suunto';
+stopwatch.shape = 'round';
+stopwatch.size = 'perfect';
+
+stopwatch.start = () => console.log('Starting');
+stopwatch.stop = () => console.log('Stopping');
+stopwatch.sayHi = () => console.log('Hi');
