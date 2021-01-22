941b0a9d0b8ac1c681a3969ea6917ff54f45169b
Daawa-Karma <theo.leveque@gmail.com>
2021-01-21 20:45:00 -0800

    lab04 done

--- class18/objects-lost-galaxy/js/main.js
+++ class18/objects-lost-galaxy/js/main.js
@@ -1,1 +1,11 @@
 //Create a mouse object that has four properties and three methods
+const mouse = {}
+
+mouse.color = 'grey'
+mouse.shape = 'cool'
+mouse.hasrgb = true
+mouse.buttons = 4
+
+mouse.leftClick = () => console.log('left click')
+mouse.righClick = () => console.log('right click')
+mouse.scroll = () => console.log('just scrolling')
