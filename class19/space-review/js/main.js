415b8a9d380ded23c6e9d5d68ac67a38cd4c0edd
Daawa-Karma <theo.leveque@gmail.com>
2021-01-29 13:52:58 -0800

    class20 started

--- class19/space-review/js/main.js
+++ class19/space-review/js/main.js
@@ -1,12 +1,17 @@
 //Arrays
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
+const array = [2, 7, 252, 23, 56]
+array.reduce((a, b) => a + b)
 
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
+const squared = array => array.map(x => x ** 2)
 
 //Create a function that takes string
 //Print the reverse of that string to the console
+const reverse = str => str.split('').reverse().join('')
 
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
+const palindrome = str => str === reverse(str)
--- class19/turbo-review/js/main.js
+++ class19/turbo-review/js/main.js
@@ -1,13 +1,33 @@
 // *Variables*
 // Declare a variable and assign it to a sentance as a string. Alert if the sentance is a question
-
+let sentence = 'How are you?'
+if (sentence.includes('?')) {
+	alert(sentence)
+}
 //Declare a variable, assign it a string of multiple words, replace every "jr. dev" with "software engineer", and print it to the console
-
+let string = 'I am a jr. dev'
+string.replace('jr. dev', 'software engineer')
+console.log(string)
 // *Functions*
 // Create a function that returns rock, paper, or scissors as randomly as possible
-
+const rps = () => {
+	let botChoice
+	let random = Math.random()
+	if (random < 0.3333) {
+		botChoice = 'rock'
+	} else if (random > 0.6667) {
+		botChoice = 'paper'
+	} else {
+		botChoice = 'scissors'
+	}
+	return botChoice
+}
 // *Conditionals*
 //Create a function that takes in a choice (rock, paper, or scissors) and determines if they won a game of rock paper scissors against a bot using the above function
+
+const playRound = () => {
+    let 
+}
 
 //*Loops*
 //Create a function that takes an array of choices. Play the game x times where x is the number of choices in the array. Print the results of each game to the console.
